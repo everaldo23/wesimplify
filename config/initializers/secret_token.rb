3f39fbc69c2207184ad60c5b08dda96de74ef0ec
Syahril <everaldo@umich.edu>
2014-05-30 16:46:14 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wesimplify::Application.config.secret_key_base = '4765a8ba11adf901c0b05a00021f1c81eaddd4578fe21648b1e9c282b0d5d4c11958863a174293a422d2c3abc3b843b0057062a78d1a59105d7a6baf31eef0c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wesimplify::Application.config.session_store :cookie_store, key: '_wesimplify_session'
